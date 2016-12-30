e1e0160469ed8b3942d9b07be75c1b9317345ae4
Fang <wangfang14@meituan.com>
2016-12-31 03:40:28 +0800

    finish finding class

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
+
 import "fmt"
+import "strings"
+import "gojvm/classpath"
 
 
 func startJVM(cmd *Cmd)  {
+    cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
     fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+
+    className := strings.Replace(cmd.class, ".", "/", -1)
+    classData, _, err := cp.ReadClass(className)
+    if err != nil {
+        fmt.Printf("Cout not find or load main class %s\n", cmd.class)
+        return
+    }
+
+    fmt.Printf("class data: %v\n", classData)
 }
+
 
 func main()  {
     cmd := parseCmd()
